908cce12ac618f66640c0ca9bbe00b3708336d69
Kory Draughn <korydraughn@renci.org>
2021-10-11 16:30:56 -0400

    Cleaned up code.

--- tests/test_list.py
+++ tests/test_list.py
@@ -1,16 +1,3 @@
-import pytest
-
-#@pytest.mark.parametrize(('path', 'expected_permission'), (
-#    ('/create?collection=/tempZone/home/kory', 'read'),
-#    ('/create?collection=/tempZone/home/kory&permission=write', 'write')
-#))
-
-#def test_list(client):
-#    response = client.post(path, headers={'authorization': 'Basic a29yeTpyb2Rz'})
-#    assert response.status_code == 200
-#    assert len(response.data) > 0
-#
-#    response = client.post('/list', headers={'authorization': 'Basic a29yeTpyb2Rz'})
-#    assert response.status_code == 200
-#    assert '"permission":"{}"'.format(expected_permission) in str(response.data)
+def test_list(client):
+    pass
 
